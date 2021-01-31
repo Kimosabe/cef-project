73ec80f21b337727a803ed7d8cb16abbd5c86b2f
Sergei Rashitov <4b.69.6d.6f@gmail.com>
2021-01-31 18:15:14 +0700

    Proxy for window.chrome

--- examples/minimal/app_renderer_minimal.cc
+++ examples/minimal/app_renderer_minimal.cc
@@ -6,9 +6,45 @@
 
 namespace shared {
 
+// Implementation of CefApp for the renderer process.
+class RendererApp : public CefApp, public CefRenderProcessHandler {
+ public:
+  RendererApp() {}
+
+  // CefApp methods:
+  CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE {
+    return this;
+  }
+
+  // CefRenderProcessHandler methods:
+  void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) {
+    std::string extensionCode =
+        "function __init() {\n"
+        "var handler3 = {\n"
+        "  get: function (target, prop, receiver) {\n"
+        "    console.log('Getter', prop);\n"
+        "    return Reflect.get(...arguments);\n"
+        "  },\n"
+        "};\n"
+        "\n"
+        "window.chrome = new Proxy(window.chrome, handler3);\n"
+        "}\n"
+        "\n"
+        "document.addEventListener('DOMContentLoaded', __init);\n";
+    if (frame->IsMain()) {
+      frame->ExecuteJavaScript(extensionCode, CefString(), 0);
+    }
+  }
+
+ private:
+
+  IMPLEMENT_REFCOUNTING(RendererApp);
+  DISALLOW_COPY_AND_ASSIGN(RendererApp);
+};
+
 // No CefApp for the renderer subprocess.
 CefRefPtr<CefApp> CreateRendererProcessApp() {
-  return NULL;
+  return new RendererApp();
 }
 
 }  // namespace shared
